964d4fdd8e241bfa6b5d89d2273218fcf254b1fd
lxh <1208401731@qq.com>
2024-11-22 06:21:07 -0600

    init

--- blog/index.js
+++ blog/index.js
@@ -4,4 +4,15 @@ import "../global.css";
 import "./index.css";
 import 'highlight.js/styles/github.css';
 
-const buttons = document.querySelectorAll("#left-section4 > ul > li > button");
+document.querySelector("#left-section1 svg:first-child").addEventListener("click", (e) => {
+    // 下拉框
+});
+document.querySelector("#left-section1 svg:last-child").addEventListener("click", (e) => {
+    // 侧边栏隐藏/显示
+});
+document.querySelector("#left-section2 h4").addEventListener("click", (e) => {
+    // 标题变更
+});
+document.querySelector("#left-section3 .search > button").addEventListener("click", (e) => {
+    // search
+});
